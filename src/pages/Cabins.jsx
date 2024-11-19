8bedac112aafecf12eedf35298c7f47e02c75c8a
Rununu Games <ngugikerei@gmail.com>
2024-11-19 08:26:14 +0300

    fix add cabin query

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showCabinForm, setShowCabinForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -13,7 +16,13 @@ function Cabins() {
       </Row>
 
       <Row>
-        <CabinTable></CabinTable>
+        <CabinTable />
+        <Button onClick={() => setShowCabinForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+        {showCabinForm && (
+          <CreateCabinForm setShowCabinForm={setShowCabinForm} />
+        )}
       </Row>
     </>
   );
