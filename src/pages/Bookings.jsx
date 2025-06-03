111391f696bf2119db175a8c92a8fe6949bf30c1
Rununu Games <ngugikerei@gmail.com>
2025-06-03 23:11:34 +0300

    fix dashboard

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -17,3 +17,4 @@ function Bookings() {
 }
 
 export default Bookings;
+  
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,19 @@
+import { Calendar } from "antd";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <p>TEST</p>
+      </Row>
+
+      <Row type="horizontal">
+        <Calendar></Calendar>
+      </Row>
+    </>
   );
 }
 
